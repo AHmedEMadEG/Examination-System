ce03e48cc309b5e81a2fd2c3c463b3de20413a6b
AHmedEMadEG <amoemo141996@gmail.com>
2024-07-16 16:26:50 +0300

    result page updated

--- logic/scripts/result.js
+++ logic/scripts/result.js
@@ -1,7 +1,19 @@
-// let loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
+let loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
-// !loggedInUser && location.replace("../../pages/index.html");
+!loggedInUser && location.replace("../../pages/index.html");
 
-// $(".score").text(loggedInUser.lastScore);
+let mess = "";
 
-// localStorage.removeItem('loggedInUser');
+(loggedInUser.lastScore >= 6) 
+  ?( mess += `Congratulations ${loggedInUser.firstName} ${loggedInUser.lastName}, You successfully passed the exam`) 
+  : mess += `${loggedInUser.firstName} ${loggedInUser.lastName}, Unfortunately you have failed the exam`;
+
+$(".message__content").text(mess);
+$(".message__img")[0].src = `../../images/${(loggedInUser.lastScore >= 6) ? "success" : "fail"}.png`;
+
+$(".score__number__content").text(loggedInUser.lastScore);
+$(".score__percentager__content").text(loggedInUser.lastScore * 10);
+
+localStorage.removeItem('loggedInUser');
+
+
